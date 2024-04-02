af558dcba9299ec242ae66f6f2d5449c7eb4690e
Yaroslav <yibelyaev@icloud.com>
2024-04-02 21:01:33 +0300

    update task 3-4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -2,7 +2,5 @@ function showSalary(users, age) {
   // ваш код...
   const filtered = users.filter((user) => user.age <= age);
 
-  return filtered.map((user) => {
-    return `${user.name}, ${user.balance}`;
-  }).join('\n');
+  return filtered.map((user) => `${user.name}, ${user.balance}`).join('\n');
 }
