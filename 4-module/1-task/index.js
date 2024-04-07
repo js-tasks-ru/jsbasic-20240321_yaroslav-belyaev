d0276d7530028be01b53a4649966e1f01750e8d6
Yaroslav <yibelyaev@icloud.com>
2024-04-07 18:08:48 +0300

    added 4 module and refact 3 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const container = document.createElement("ul");
+
+  for (let i = 0; i < friends.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = `${friends[i].firstName} ${friends[i].lastName}`;
+
+    container.append(li);
+  }
+
+  return container;
 }
