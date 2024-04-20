f1fc12af0972a28ef313bbe0eccdff774f3eaa26
Yaroslav <yibelyaev@icloud.com>
2024-04-20 11:33:47 +0300

    added 6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,48 @@
  *
  */
 export default class UserTable {
+  elem = null;
+  #list = [];
+
   constructor(rows) {
+    this.#list = rows;
+
+    this.elem = this.#render();
+  }
+
+  #template() {
+    return `
+    <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        ${this.#list.map((value) => `<tr>${this.#templateCell(value)}</tr>`)}
+      </tbody>
+    `;
+  }
+
+  #templateCell(obj) {
+    let values = Object.values(obj);
+    values.push(`<td><button class="btn-delete">X</button></td>`);
+    return values.map(value => `<td>${value}</td>`);
+  }
+
+  #onClickRemove = (e) => {
+    e.target.closest('tr').remove();
+  }
+
+  #render() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = this.#template();
+
+    this.elem.addEventListener('click', this.#onClickRemove);
+
+    return this.elem;
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,45 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class ProductCard {
+  elem = null;
+  #product = {};
   constructor(product) {
+    this.#product = product || this.#product;
+
+    this.elem = this.#render();
+  }
+
+  #template() {
+    return `
+            <div class="card__top">
+              <img src="/assets/images/products/${this.#product.image}" class="card__image" alt="product">
+              <span class="card__price">€${this.#product.price.toFixed(2)}</span>
+            </div>
+            <div class="card__body">
+              <div class="card__title">${this.#product.name}</div>
+                <button type="button" class="card__button">
+                  <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                </button>
+              </div>
+      `;
+  }
+
+  #onClickAdd = () => {
+    const event = new CustomEvent('product-add', {
+      bubbles: true,
+      detail: this.#product.id,
+    });
+
+    this.elem.querySelector('.card__button').dispatchEvent(event);
+  }
+
+  #render() {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('card');
+    this.elem.innerHTML = this.#template();
+
+    this.elem.querySelector('.card__button').addEventListener('click', this.#onClickAdd);
+
+    return this.elem;
   }
 }
