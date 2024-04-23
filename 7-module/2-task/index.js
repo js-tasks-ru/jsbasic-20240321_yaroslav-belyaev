cbaf4cdc4b66b93d8ba5a456673ad329ac0abe7e
Yaroslav <yibelyaev@icloud.com>
2024-04-23 20:11:24 +0300

    finish 72

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,71 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
+  elem = null;
   constructor() {
+    this.elem = this.render();
   }
+
+  #template() {
+    return `
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 class="modal__title"></h3>
+          </div>
+          <div class="modal__body"></div>
+        </div>
+    `;
+  }
+
+  setTitle(title) {
+    this.elem.querySelector('.modal__title').textContent = title;
+  }
+
+  setBody(node) {
+    this.elem.querySelector('.modal__body').innerHTML = '';
+    this.elem.querySelector('.modal__body').append(node);
+  }
+
+
+  open() {
+    document.body.append(this.elem);
+    document.body.classList.add('is-modal-open');
+    document.addEventListener('keydown', this.#onKeyDown);
+  }
+
+
+  close() {
+    document.body.classList.remove('is-modal-open');
+    document.removeEventListener('keydown', this.#onKeyDown);
+    this.elem.remove();
+  }
+
+  #onClick = () => {
+    this.elem.querySelector('.modal__close').addEventListener('click', () => {
+      this.close();
+    }, {once: true});
+  }
+
+
+  #onKeyDown = (event) => {
+    if (event.code === 'Escape') {
+      event.preventDefault();
+      this.close();
+    }
+  };
+
+  render() {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('modal');
+    this.elem.innerHTML = this.#template();
+
+    this.#onClick();
+
+    return this.elem;
+  }
+
 }
